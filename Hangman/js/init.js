dc8cc054f8631e387c601655fe1ed9fa1bfbaa52
Ryan <datmanwich@gmail.com>
2018-10-16 09:49:03 -0700

    Added initScore function that displays the users score

--- Hangman/js/init.js
+++ Hangman/js/init.js
@@ -1,10 +1,16 @@
 // Initializes the hangman game
 function init() {
+  initScore();
   document.getElementById("guess").innerHTML = "Guess the word, it's " + randLength + " letters long";
   underscore();
   initDef();
   countWord();
   underWord();
+}
+
+function initScore() {
+  let score = document.getElementById("plzwork").value;
+  document.getElementById("score").innerHTML = score;
 }
 
 init();
